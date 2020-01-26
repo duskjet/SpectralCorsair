c589680f30e1b68e5b95e236a4e41ef079c17407
Ivan Belikov <ivan.belikov@docu.se>
2020-01-27 01:16:54 +0200

    Remove command line arguments, Add Extensions.Configuration

--- SpectralCorsair/Program.cs
+++ SpectralCorsair/Program.cs
@@ -1,10 +1,12 @@
-﻿using SixLabors.Fonts;
+﻿using Microsoft.Extensions.Configuration;
+using SixLabors.Fonts;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats.Jpeg;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 using SixLabors.Primitives;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading;
@@ -16,17 +18,31 @@ namespace SpectralCorsair
 {
     class Program
     {
+        private const string AccessTokenKey = "Telegram:AccessToken";
         static TelegramBotClient bot;
 
         static async Task Main(string[] args)
         {
+            IConfiguration config = new ConfigurationBuilder()
+                .AddCommandLine(args, new Dictionary<string, string> { 
+                    ["-t"] = AccessTokenKey, 
+                    ["--access-token"] = AccessTokenKey 
+                })
+                .AddUserSecrets<Program>()
+                .Build();
+
             CancellationTokenSource cts = new CancellationTokenSource();
             Console.CancelKeyPress += (_, e) => {
                 e.Cancel = true; // prevent the process from terminating.
                 cts.Cancel();
             };
 
-            string access_token = args[0];
+            string access_token = config.GetValue<string>(AccessTokenKey);
+            if (string.IsNullOrEmpty(access_token))
+            {
+                throw new InvalidOperationException("Telegram access token must be set.");
+            }
+
             bot = new TelegramBotClient(access_token);
 
             var me = await bot.GetMeAsync(cts.Token);
